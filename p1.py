1237f81ffab06410d3256394e1ed772a745803bd
SuyogNe <suyogn78@gmail.com>
2025-04-16 21:49:34 +0545

    p1

---
+++ p1.py
@@ -0,0 +1,9 @@
+def printnumber(n):
+    iteration=0
+    print("The number is: ",n)
+    iteration += 1
+    print("The iteration is: ",iteration)
+
+printnumber(5)
+printnumber(10)
+print("\n the time taken wont change with any n value")
