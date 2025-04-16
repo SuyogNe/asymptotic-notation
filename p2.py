6e86d459d52f7cf4ac3be2a5082109f7cf77ee6b
SuyogNe <suyogn78@gmail.com>
2025-04-16 22:01:26 +0545

    p2

---
+++ p2.py
@@ -0,0 +1,11 @@
+def ontime(n):
+    iteration=0
+    for i in range(1,n+1):
+        iteration += 1
+    print("when n is iterated, no of time is: ",iteration)
+
+ontime(5)
+ontime(10)
+ontime(100)
+
+print("\n the time taken will increase with n value")
